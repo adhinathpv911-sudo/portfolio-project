9f2b3b940f51256604524cc32bb140353cd9b7e1
adhinathpv911-sudo <adhinathpv911@gmail.com>
2026-03-23 21:37:14 +0530

    modern ui + project cards

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 async function sendMessage() {
   const name = document.getElementById("name").value;
   const message = document.getElementById("message").value;
+  const status = document.getElementById("status");
 
-  await fetch("https://portfolio-project-nj1q.onrender.com/message", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ name, message }),
-  });
+  if (!name || !message) {
+    status.innerText = "Please fill all fields ❗";
+    return;
+  }
 
-  alert("Message sent!");
+  try {
+    await fetch("https://portfolio-project-nj1q.onrender.com/message", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, message }),
+    });
+
+    status.innerText = "Message sent successfully ✅";
+
+    document.getElementById("name").value = "";
+    document.getElementById("message").value = "";
+  } catch (error) {
+    status.innerText = "Error sending message ❌";
+  }
 }
